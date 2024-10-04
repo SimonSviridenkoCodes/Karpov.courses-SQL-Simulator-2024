6a298058f25eaf4f80fd4d0028f2d49976410f2b
Simon Sviridenko <simonsviridenko@gmail.com>
2024-10-04 06:39:38 +0300

    Tasks 5.13-5.15

---
+++ Module 1 Lesson 5/SQL queries/Module 1 Task 5.15.sql
@@ -0,0 +1,14 @@
+/*На основе данных в таблице user_actions посчитайте количество уникальных пользователей сервиса,
+количество уникальных заказов, поделите одно на другое и выясните, сколько заказов приходится на одного пользователя.
+В результирующей таблице отразите все три значения — поля назовите соответственно unique_users, unique_orders, orders_per_user.
+Показатель числа заказов на пользователя округлите до двух знаков после запятой.
+Поля в результирующей таблице: unique_users, unique_orders, orders_per_user*/
+
+SELECT
+  COUNT(DISTINCT user_id) AS unique_users,
+  COUNT(DISTINCT order_id) AS unique_orders,
+  ROUND (COUNT(DISTINCT order_id) :: DECIMAL/ COUNT(DISTINCT user_id :: DECIMAL),2)  AS orders_per_user
+FROM
+  user_actions
+  
+  /*https://redash.public.karpov.courses/queries/66960/source*/
